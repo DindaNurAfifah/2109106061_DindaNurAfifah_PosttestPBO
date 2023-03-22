b543405d9f65717d5706929be78678b4c5a30731
Dinda Nur Afifah <92873925+akabanelay@users.noreply.github.com>
2023-03-22 15:11:43 +0800

    Add files via upload

---
+++ Posttest2PBO/src/posttest2pbo/Posttest2PBO.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package posttest2pbo;
+
+/**
+ *
+ * @author USER
+ */
+public class Posttest2PBO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
